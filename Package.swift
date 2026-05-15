ec5f5f9be5f485a3525535979c550cace272b6a0
Hareem Adderley <hadderley@kingpinapps.com>
2026-05-15 06:43:08 -0500

    fix: improve swift version compatibility

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,9 +17,7 @@ let package = Package(
             targets: ["SwiftOgmios"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.3.19"),
-        // Cross-platform WebSocket client (Apple + Linux); replaces URLSessionWebSocketTask
-        // which is unavailable in swift-corelibs-foundation on Linux.
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.3.21"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.16.2"),
     ],
     targets: [
