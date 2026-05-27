2a67d00d0a0d805262808dbd3f4e0fe62b83acb9
Hareem Adderley <hadderley@kingpinapps.com>
2026-05-26 20:57:10 -0500

    chore: bump swift-cardano-core to 0.4.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["SwiftOgmios"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.3.21"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.4.0"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.16.2"),
     ],
     targets: [
